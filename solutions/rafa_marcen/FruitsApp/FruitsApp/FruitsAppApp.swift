f5e683bb26c733fd088f50cacb76688752393c63
Arafo <rafa_marcen@hotmail.com>
2022-09-23 11:21:12 +0200

    Rafa's branch

---
+++ solutions/rafa_marcen/FruitsApp/FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Jamous, Georges (formerly Georges) on 20/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
